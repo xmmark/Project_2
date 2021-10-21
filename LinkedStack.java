fd9f47b44b9b7c48b1d11ee45d63bca62e46bc75
xvrmrkwtz <xmmark23@colby.edu>
2021-10-21 00:10:14 -0400

    added push method to LinkedStack

---
+++ LinkedStack.java
@@ -0,0 +1,39 @@
+ public class LinkedStack<T>{  
+
+    public class Node<T>{
+        public T data;
+        public Node<T> next;
+        
+        public Node(T value){
+            this.data = value;
+            this.next = null;
+        }
+    }
+
+    private Node<T> head;
+    private int size = 0;
+
+    public void push(T data){
+        if(size == 0){
+            Node<T> newNode = new Node<T>(data);
+            head = newNode;
+            size++;
+        }else{
+            Node<T> newNode = new Node<T>(data);
+            newNode.next = head;
+            head = newNode;
+            size++;
+        }
+    }
+
+    public static void main(String[] args){
+
+    LinkedStack<String> stringStack = new LinkedStack<>();
+    //LinkedStack<String>.Node<String> stringNode = new LinkedStack<String>.Node<String>();
+    stringStack.push("A");
+    stringStack.push("B");
+    System.out.println(stringStack.head.data);
+    System.out.println(stringStack.head.next.data);
+    }
+
+}
---
+++ linked stack.java
@@ -0,0 +1,6 @@
+
+public class Node{
+    public T data;
+    public Node next;
+    
+}
